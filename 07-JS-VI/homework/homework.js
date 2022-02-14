bd6579be36f443785d8ee7f2a4c21ebb40e2d380
Ailín T <ailin.tobares@gmail.com>
2022-02-14 11:12:29 -0300

    Homework JS VI completo

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -4,17 +4,27 @@ function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
+
+  let letra = nombre.charAt(0).toUpperCase();
+  let cuerpo = nombre.slice(1);
+
+  return letra + cuerpo;
 }
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
   //Tu código:
+
+  return cb();
 }
 
 function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
+
+   return cb(n1, n2);
+
 }
 
 function sumarArray(numeros, cb) {
@@ -22,12 +32,21 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
+
+  let suma = numeros.reduce(function(acumulador, valorActual){
+  return acumulador + valorActual;
+  }, 0);
+  cb(suma);
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
+
+  array.forEach(function(val, index) {
+  cb(val);
+  });
 }
 
 function map(array, cb) {
@@ -35,12 +54,31 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
+
+  let nuevoArray = array.map(function(valor){
+    return cb(valor);
+  });
+  return nuevoArray;
 }
 
 function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
+
+ /* ESTE MÉTODO FUNCIONA PERO EL TEST NO LO ACEPTA
+     function empiezaConA(array) {
+       if (array[0] === 'a'){
+        return true;
+      } else { return false; }
+     }
+       let nuevoArray = array.filter(empiezaConA); */
+  
+let nuevoArray = [];
+for (let i = 0; i < array.length; i++) {
+  if (array[i][0] === 'a') nuevoArray.push(array[i])
+}
+return nuevoArray;
 }
 
 // No modificar nada debajo de esta línea
